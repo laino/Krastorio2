a3219577d4fd5b4182213e4f1af2a87e27e3db0b
Caleb Heuer <ch@raiguard.me>
2023-03-11 01:57:27 -0700

    Compare data.raw against previous commit

--- verify-data-raw.sh
+++ verify-data-raw.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
+set -e
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Commit changes first"
+    exit 1
+fi
+
+commit_delta="$1"
+if [ -z "$commit_delta" ]; then
+    commit_delta="1"
+fi
+
 outdir="$(dirname $FACTORIO)/../../script-output"
-getsum() {
+dump() {
     $FACTORIO --dump-data > /dev/null
     echo $(md5sum "$outdir/data-raw-dump.json" | awk '{ print $1 }')
 }
 
-fmm e Krastorio2_1.3.16
-oldsum=$(getsum)
-mv $outdir/data-raw-dump.json $outdir/data-raw-dump-1-3.json
-fmm e Krastorio2_2.0.0
-newsum=$(getsum)
+currentsum=$(dump)
+mv "$outdir/data-raw-dump.json" "$outdir/data-raw-dump-head.json"
+git checkout "HEAD~$commit_delta" -q
+oldsum=$(dump)
+git checkout 2.0-new -q
 
-if [ "$newsum" == "$oldsum" ]; then
+if [ "$currentsum" == "$oldsum" ]; then
     echo "PASSED"
 else
     echo "FAILED"
-    git diff --no-index $outdir/data-raw-dump-1-3.json $outdir/data-raw-dump.json
+    git diff --no-index "$outdir/data-raw-dump.json" "$outdir/data-raw-dump-head.json"
 fi
